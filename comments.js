f3099a2d395d4c135cc2dd55d52044941b94d18e
jesse <web.jesse@gmail.com>
2023-05-06 23:40:46 +0800

    t3

---
+++ comments.js
@@ -0,0 +1,78 @@
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the comments data
+const comments = require('./data/comments');
+
+// Path: movies.js
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the movies data
+const movies = require('./data/movies');
+
+// Path: products.js
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the products data
+const products = require('./data/products');
+
+// Path: vehicles.js
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the vehicles data
+const vehicles = require('./data/vehicles');
+
+// Path: vehicles.js
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the vehicles data
+const vehicles = require('./data/vehicles');
+
+// Path: vehicles.js
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the vehicles data
+const vehicles = require('./data/vehicles');
+
+// Path: vehicles.js
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the vehicles data
+const vehicles = require('./data/vehicles');
+
+// Path: vehicles.js
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the vehicles data
+const vehicles = require('./data/vehicles');
+
+// Path: vehicles.js
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the vehicles data
+const vehicles = require('./data/vehicles');
+
+// Path: vehicles.js
+// Create web server application with express
+const express = require('express');
+const app = express();
+
+// Import the vehicles data
+const vehicles = require('./data/vehicles');
